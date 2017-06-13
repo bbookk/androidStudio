18faa4857eda24e1b2ca545d85b0166bb827e640
bbookk <book.maytawee@gmail.com>
2017-06-13 10:12:59 +0700

    Sudoku game with android

--- Sudoku/app/src/main/java/com/example/office1/sudoku/Keypad.java
+++ Sudoku/app/src/main/java/com/example/office1/sudoku/Keypad.java
@@ -1,8 +1,125 @@
 package com.example.office1.sudoku;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.app.Dialog;
+import android.widget.Toast;
+
 /**
  * Created by OFFICE1 on 12/06/2560.
  */
 
-public class Keypad {
+public class Keypad extends Dialog{
+    protected static final String TAG = "Sudoku";
+    //อาเรย์ที่ใช้สำหรับอ้างอิงไปยังปุ่มต่างๆ
+    private final View keys[] = new View[10];
+    //อาเรย์ที่เก็บตัวเลขที่ไม่สามารถเติมลงบนช่องเคอเซอร์ได้
+    private final int useds[];
+    private final PuzzleView puzzleView;
+
+
+    public Keypad(Context context, int useds[], PuzzleView puzzleView) {
+        super(context);
+        this.useds = useds;
+        this.puzzleView = puzzleView;
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.keypad_title);
+        setContentView(R.layout.keypad);
+        findView();
+        for(int element : useds){
+            keys[element].setVisibility(View.INVISIBLE);
+        }
+        setListeners();
+    }
+
+    public  void findView(){
+        keys[0] = findViewById(R.id.keypad_clear);
+        keys[1] = findViewById(R.id.keypad_1);
+        keys[2] = findViewById(R.id.keypad_2);
+        keys[3] = findViewById(R.id.keypad_3);
+        keys[4] = findViewById(R.id.keypad_4);
+        keys[5] = findViewById(R.id.keypad_5);
+        keys[6] = findViewById(R.id.keypad_6);
+        keys[7] = findViewById(R.id.keypad_7);
+        keys[8] = findViewById(R.id.keypad_8);
+        keys[9] = findViewById(R.id.keypad_9);
+    }
+
+    private void setListeners(){
+        for(int i = 0; i < keys.length; i++){
+            final int t = i;
+            keys[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    returnResult(t);
+                }
+            });
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        int tile = 0;
+        switch(keyCode){
+            case KeyEvent.KEYCODE_0:
+            case KeyEvent.KEYCODE_SPACE:
+                tile = 0;
+                break;
+            case KeyEvent.KEYCODE_1:
+                tile = 1;
+                break;
+            case KeyEvent.KEYCODE_2:
+                tile = 2;
+                break;
+            case KeyEvent.KEYCODE_3:
+                tile = 3;
+                break;
+            case KeyEvent.KEYCODE_4:
+                tile = 4;
+                break;
+            case KeyEvent.KEYCODE_5:
+                tile = 5;
+                break;
+            case KeyEvent.KEYCODE_6:
+                tile = 6;
+                break;
+            case KeyEvent.KEYCODE_7:
+                tile = 7;
+                break;
+            case KeyEvent.KEYCODE_8:
+                tile = 8;
+                break;
+            case KeyEvent.KEYCODE_9:
+                tile = 9;
+                break;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+        if(isValid(tile)){
+            returnResult(tile);
+        }
+        return true;
+    }
+
+    private boolean isValid(int tile){
+        for(int t : useds){
+            if(tile == t){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void returnResult(int tile){
+        puzzleView.setSelectedTile(tile);
+        dismiss();
+    }
+    
 }
--- Sudoku/app/src/main/java/com/example/office1/sudoku/PuzzleActivity.java
+++ Sudoku/app/src/main/java/com/example/office1/sudoku/PuzzleActivity.java
@@ -1,7 +1,10 @@
 package com.example.office1.sudoku;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class PuzzleActivity extends Activity {
     protected static final String KEY_DIFFICULTY = "level_game";
@@ -19,12 +22,153 @@ public class PuzzleActivity extends Activity {
         int diff =getIntent().getIntExtra(KEY_DIFFICULTY,EASY);
 
         puzzle = getPuzzle(diff);
-        calculatedUsedTiles();
+        calculateUsedTiles();
 
         puzzleView = new PuzzleView(this);
         setContentView(puzzleView);
         puzzleView.requestFocus();
-
-
     }
+
+    protected boolean setTileIfValid(int x, int y, int value){
+        int tiles[] = getUsedTiles(x, y);
+
+        if(value != 0){
+            for(int tile : tiles){
+                //ถ้าตัวเลขที่ต้องการเติมถูกใช้ไปแล้ว ให้ออกจาเมธอดทันที
+                if(tile == value){
+                    return false;
+                }
+
+            }
+        }
+        setTile(x, y, value);
+        calculateUsedTiles();
+        return true;
+    }
+
+    private final int used[][][] = new int[9][9][];
+
+    protected int[] getUsedTiles(int x, int y){
+        return used[x][y];
+    }
+
+    private void calculateUsedTiles(){
+        for(int x = 0; x < 9; x++){
+            for(int y = 0; y < 9; y++){
+                used[x][y] = calculateUsedTiles(x, y);
+            }
+        }
+    }
+
+    private int[] calculateUsedTiles(int x, int y){
+        int c[] = new int[9];
+
+        for(int i = 0; i < 9; i++){
+            if(i == y){
+                continue;
+            }
+            int t = getTile(x, i);
+            if(t != 0)
+                c[t - 1] = t;
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(i == x){
+                continue;
+            }
+            int t = getTile(i, y);
+            if(t != 0)
+                c[t - 1] = t;
+        }
+
+        int startx = (x/3)*3;
+        int starty = (x/3)*3;
+        for(int i = startx; i < startx+3; i++){
+            for(int j = starty; j < starty+3; j++){
+                if(i == x && j == y)
+                    continue;
+                int t = getTile(i, j);
+                if(t != 0)
+                    c[t - 1] = t;
+            }
+        }
+
+        int nused = 0;
+        for(int t : c){
+            if(t != 0){
+                nused++;
+            }
+        }
+
+        int c1[] = new int[nused];
+        nused = 0;
+        for(int t : c){
+            if(t != 0){
+                c1[nused++] = t;
+            }
+        }
+
+        return c1;
+    }
+
+    private void setTile(int x, int y, int value){
+        puzzle[y * 9 + x] = value;
+    }
+
+    private int getTile(int x, int y){
+        return puzzle[y * 9 + x];
+    }
+
+    protected String getTileString(int x, int y){
+        int v = getTile(x, y);
+        if(v == 0){
+            return "";
+        }
+        else{
+            return String.valueOf(v);
+        }
+    }
+
+    private final String easy = "000070000080000273009036800" + "001247000807050309000389400" + "003510700124000090000020000";
+    private final String medium = "300000056400000000000897000" + "002080400006103800005070300" + "000462000000000008510000007";
+    private final String hard = "46290000900100000705800000" + "631000000000000000000000259" + "000005901000008003000001542";
+
+    private int[] getPuzzle(int diff){
+        String puz = null;
+        switch(diff){
+            case HARD:
+                puz = hard;
+                break;
+            case MEDIUM:
+                puz = medium;
+                break;
+            case EASY:
+                puz = easy;
+                break;
+        }
+        return fromPuzzleString(puz);
+    }
+
+    static protected int[] fromPuzzleString(String str){
+        int[] puz =new int[str.length()];
+        for(int i = 0; i < puz.length; i++){
+            puz[i] = str.charAt(i) - '0';
+        }
+        return puz;
+    }
+
+    protected void showKeypadOrError(int x, int y){
+        int tiles[] = getUsedTiles(x, y);
+        if(tiles.length == 9){
+            Toast toast = Toast.makeText(this, R.string.no_move, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+        else{
+            Dialog v = new Keypad(this, tiles, puzzleView);
+            v.show();
+        }
+    }
+
+
 }
